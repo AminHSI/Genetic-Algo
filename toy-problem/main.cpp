631a428b6c63d06951a55561aebaae96a6223b1f
AminHSI <aminhsikpk@gmail.com>
2024-04-27 23:16:21 +0200

    evaluate function made

--- toy-problem/main.cpp
+++ toy-problem/main.cpp
@@ -1,30 +1,75 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <random>
 #include <array>
-#include <list>
 using namespace std;
 
 
-int randomnum() {
+int randomnum () {
+
     random_device r;
+
     uniform_int_distribution<int> number(0,9);
+
+    return number(r);
 }
 
-int sort(int numbers) {
+
+void evaluate (int(&numbers)[40][5], int target[4]) {
+    
+    for (int i=0 ; i<40 ; i++) {
+        
+        int counter = 0;
+
+        for (int j=0 ; j<4 ; j++){
+            
+            if (numbers[i][j] == target [j]) {
+                counter ++;
+            }
+            numbers[i][4] = counter;
+        }
+    }
+}
+
+
+void sort(int(&numbers)[40][5]) {
 
 }
+
 
 int main() {
+
     cout << "enter a 4-digit number: ";
-    string target;
-    cin >> target;
+
+    int target[4];
+
+    for(int i=0 ; i<4 ; i++){
+        cin >> target[i];
+    }
+
     int numbers[40][5];
-    for(int i=0;i<4;i++) {
+
+    for(int i=0;i<40;i++) {
+
         for(int j=0;j<4;j++) {
+
             numbers[i][j] = randomnum();
         }
     }
 
+    
+    evaluate(numbers, target);
+
+    for(int i=0;i<40;i++) {
+
+        for(int j=0;j<5;j++) {
+
+            cout << numbers[i][j];
+        }
+        cout << endl;
+    }
+
+    sort(numbers);
+
+    return 0;
 }
