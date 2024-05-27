a43ebfb3e9c4ee83ea37416e74420434afaff337
AminHSI <aminhsikpk@gmail.com>
2024-05-27 18:24:05 +0200

    making it more readable

--- main.cpp
+++ main.cpp
@@ -4,53 +4,49 @@
 #include <array>
 using namespace std;
 
+
+//----------------------------- configuration --------------------------------
+
 const int init_population = 100;
-
 const int repeat = 10;
+const float mutation_rate = 0.5;
 
-const float population_rate = 0.5;
+//----------------------------------------------------------------------------
+//---------------------------- number generator ------------------------------
 
 int randomnum () {
-
     random_device r;
-
     uniform_int_distribution<int> number(0,9);
-
     return number(r);
 }
 
+//----------------------------------------------------------------------------
+//---------------------------- fintness function -----------------------------
 
-
-
-void evaluate (int(&numbers)[init_population][5], int target[4]) {
-    
+void evaluate (int(&numbers)[init_population][5], int target[4]) {   
     for (int i=0 ; i<init_population ; i++) {
         
         int counter = 0;
  
-        for (int j=0 ; j<4 ; j++){
-            
+        for (int j=0 ; j<4 ; j++){           
             if (numbers[i][j] == target [j]) {
                 counter ++;
             }
             numbers[i][4] = counter;
         }
-
     }
 }
 
+//----------------------------------------------------------------------------
+//--------------------------------- sort -------------------------------------
 
 void sort(int(&numbers)[init_population][5]) {
-
     for(int i=0 ; i<init_population ; i++) {
 
         int maxvalue[2] = {i, numbers[i][4]};
         
-
         for(int j=i ; j<init_population ; j++) {
-
             if(numbers[j][4] > maxvalue[1]) {
-
                 maxvalue[0] = j; maxvalue[1] = numbers[j][4];
             }
         }
@@ -66,13 +62,13 @@ void sort(int(&numbers)[init_population][5]) {
         for(int j=0 ; j<5 ; j++) {
             numbers[maxvalue[0]][j] = tempnum[j];
         }
-
     }
 }
 
+//----------------------------------------------------------------------------
+//------------------------------ cross over ----------------------------------
 
 void cross_over(int(&numbers)[init_population][5]){
-
     for (int i=0 ; i<init_population/2 ; i+=2){
 
         numbers[i + (init_population/2)][0] = numbers[i][0];
@@ -87,86 +83,73 @@ void cross_over(int(&numbers)[init_population][5]){
     }
 }
 
+//----------------------------------------------------------------------------
+//-------------------------------- print -------------------------------------
 
 void print(int(&numbers)[init_population][5]) {
-
     for (int i =0 ; i<init_population ; i++) {
-
         for (int j =0 ; j<5 ; j++){
-
             cout << numbers[i][j];
         }   
         cout << endl;
     } 
 }
 
+//----------------------------------------------------------------------------
+//------------------------------- Mutation -----------------------------------
 
-void mutation(int(&numbers)[init_population][5]){
-    
-    for(int i=0 ; i< int(init_population * population_rate) ; i++) {
-
+void mutation(int(&numbers)[init_population][5]){    
+    for(int i=0 ; i< int(init_population * mutation_rate) ; i++) {
         numbers[randomnum()][randomnum()] = randomnum();
     }
 }
 
+//----------------------------------------------------------------------------
+//-------------------------------- M A I N -----------------------------------
+//----------------------------------------------------------------------------
 
 int main() {
 
-    cout << "enter a 4-digit number: ";
-
+    //setting the target
     int target[4];
-
+    cout << "enter a 4-digit number: ";                          
     for(int i=0 ; i<4 ; i++){
         cin >> target[i];
     }
 
+    //creating our initial population
     int numbers[init_population][5];
-
-    for(int i=0;i<init_population;i++) {
-
+    for(int i=0;i<init_population;i++) {         
         for(int j=0;j<4;j++) {
-
             numbers[i][j] = randomnum();
         }
     }
-
     int generation = 0;
+
     
     for(int rpt=0 ; rpt < repeat ; rpt++) {
 
+        //evaluating and sorting the result
         evaluate(numbers, target);
-
         sort(numbers);
-        
-        if(numbers[0][4] == 4) {
 
+        //checking for the target
+        if(numbers[0][4] == 4) {
             cout << "target was found in generation " << generation;
             break;
 
         } else {
-
-            cout << "best case found so far: ";
-
+            cout << "best case found in generation " << generation << " is : " ;
             for (int i=0 ; i<4 ; i++) {
                 cout << numbers[0][i];
             }
             cout << endl;
 
+            //creating the next generation
             cross_over(numbers);
-
             mutation(numbers);
-
             generation++;
-        }
-        
-        
-    }
-    
-
-
-    
-
-
-    
+        }       
+    }   
     return 0;
 }
